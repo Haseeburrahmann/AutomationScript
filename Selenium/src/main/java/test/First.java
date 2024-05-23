767fb185734ba06678726583dc7968991c39c85c
Haseeb Mohammad <Haseeb.Mohammad@jigede.com>
2024-05-23 11:53:35 -0400

    First Commit

---
+++ Selenium/src/main/java/test/First.java
@@ -0,0 +1,35 @@
+package test;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class First 
+{
+  public static void main(String[] args) throws InterruptedException 
+  {
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://in.bookmyshow.com/");
+	
+	driver.manage().window().maximize();
+	//driver.manage().deleteAllCookies();
+	
+	
+	driver.manage().timeouts().getImplicitWaitTimeout();
+	driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Mumbai");
+	//driver.findElement(By.xpath("//*[@id=\"modal-root\"]/div/div/div/div[1]/div/div/input")).sendKeys("Hyderabad");
+	
+	
+	
+	
+	Alert alert = driver.switchTo().alert();
+	
+	alert.accept();
+	
+     driver.quit();
+	
+  }
+}
